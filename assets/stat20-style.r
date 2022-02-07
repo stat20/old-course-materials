b9c706b7e4f073cba1284a5c6a96f3d68ae360e1
andrewpbray <andrewbray@berkeley.edu>
2022-02-07 08:06:53 -0800

    add buffer

--- assets/stat20-style.r
+++ assets/stat20-style.r
@@ -89,13 +89,13 @@ xaringanthemer::style_duo_accent(
                                "float" = "left"),
     ".pull-right-wide" = list("width" = "70%",
                               "float" = "right"),
-    ".pull-left-forty" = list("width" = "40%",
+    ".pull-left-forty" = list("width" = "39%",
                               "float" = "left"),
-    ".pull-left-sixty" = list("width" = "60%",
+    ".pull-left-sixty" = list("width" = "59%",
                               "float" = "left"),
-    ".pull-right-forty" = list("width" = "40%",
+    ".pull-right-forty" = list("width" = "39%",
                               "float" = "right"),
-    ".pull-left-sixty" = list("width" = "60%",
+    ".pull-left-sixty" = list("width" = "59%",
                               "float" = "right"),
   )
 )
